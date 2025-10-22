3f3f50876687d5da4878c3047fe5f50a1df67f26
Roman Strynzha <roman.strynzha@gmail.com>
2025-10-22 15:54:46 +0300

    work on 22/10/25

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,15 +1,18 @@
-import {Outlet} from 'react-router-dom';
-import {Header} from '@components/Header';
-import {Footer} from '@components/Footer';
+import { Outlet } from 'react-router-dom';
+import { Header } from '@components/Header';
+import { Footer } from '@components/Footer';
+import cls from './MainLayout.module.css';
 
-export const MainLayout = () => { 
-  return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  )
-}
+export const MainLayout = () => {
+	return (
+		<div className={cls.mainLayout}>
+			<Header />
+			<div className={cls.mainWrapper}>
+				<main className={cls.main}>
+					<Outlet />
+				</main>
+				<Footer />
+			</div>
+		</div>
+	);
+};
