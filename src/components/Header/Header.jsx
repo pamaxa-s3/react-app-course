e427405e9a17bea2dfb541b3b8d15bf28286817f
Roman Strynzha <roman.strynzha@gmail.com>
2025-10-23 14:13:26 +0300

    header and button implementation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import { Button } from '@components/Button';
+import cls from './Header.module.css';
+import logo from '@assets/react.svg';
+
 export const Header = () => {
-  return (
-    <header>Header</header>
-    )
-}
+	return (
+		<header className={cls.header}>
+			<p>
+				<img src={logo} alt="logo react" />
+				<span>React Cards</span>
+			</p>
+			<div className={cls.headerButtonGroup}>
+				<Button isDisabled>Add</Button>
+				<Button isActive>Login</Button>
+			</div>
+		</header>
+	);
+};
---
+++ src/page/Forbiden/index.jsx
@@ -0,0 +1,1 @@
+export * from './Forbiden.jsx'
